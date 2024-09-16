cdc67ad644db6127bba6d0a4181be2cbbab886a9
Travis Luong <travis@travisluong.com>
2024-09-17 01:10:49 +0800

    blank strings in form data should get nulled in db

--- lib/form-data-utils.ts
+++ lib/form-data-utils.ts
@@ -2,7 +2,7 @@ import { caseFactory } from "./case-utils";
 
 export const formDataUtils = {
   integer(col: string): string {
-    return `    ${col}: parseInt(formData.get("${col}") as string),`;
+    return `    ${col}: formData.get("${col}") ? parseInt(formData.get("${col}") as string) : null,`;
   },
 
   boolean(col: string): string {
@@ -10,23 +10,23 @@ export const formDataUtils = {
   },
 
   string(col: string): string {
-    return `    ${col}: formData.get("${col}") as string,`;
+    return `    ${col}: (formData.get("${col}") as string) || null,`;
   },
 
   json(col: string): string {
-    return `    ${col}: formData.get("${col}") ? JSON.parse(formData.get("${col}") as string) : "",`;
+    return `    ${col}: formData.get("${col}") ? JSON.parse(formData.get("${col}") as string) : null,`;
   },
 
   date(col: string): string {
-    return `    ${col}: new Date(formData.get("${col}") as string),`;
+    return `    ${col}: formData.get("${col}") ? new Date(formData.get("${col}") as string) : null,`;
   },
 
   float(col: string): string {
-    return `    ${col}: parseFloat(formData.get("${col}") as string),`;
+    return `    ${col}: formData.get("${col}") ? parseFloat(formData.get("${col}") as string) : null,`;
   },
 
   bigint(col: string): string {
-    return `    ${col}: BigInt(formData.get("${col}") as string),`;
+    return `    ${col}: formData.get("${col}") ? BigInt(formData.get("${col}") as string) : null,`;
   },
 
   references(col: string): string {
