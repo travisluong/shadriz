acff94ae63ec54bb313175957356b6b8bcc5ed5b
Travis Luong <travis@travisluong.com>
2024-09-23 18:49:36 +0800

    support dark mode for docs

--- templates/docs/app.js
+++ templates/docs/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtn = document.querySelector("#menu-btn");
   const sidebar = document.querySelector(".shadriz-sidebar");
+  const darkBtn = document.querySelector("#dark-btn");
   menuBtn.addEventListener("click", () => {
     console.log("menu clicked");
 
@@ -14,6 +15,16 @@ document.addEventListener("DOMContentLoaded", () => {
   sidebar.addEventListener("click", (e) => {
     if (e.target.closest("a")) {
       sidebar.classList.remove("active");
+    }
+  });
+
+  darkBtn.addEventListener("click", () => {
+    if (document.body.classList.contains("dark")) {
+      document.body.classList.remove("dark");
+      darkBtn.innerHTML = "Dark";
+    } else {
+      document.body.classList.add("dark");
+      darkBtn.innerHTML = "Light";
     }
   });
 });
