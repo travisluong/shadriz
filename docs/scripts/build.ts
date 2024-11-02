e924d3e891fdbd6f2e99f5996f9746a5f7eab41e
Travis Luong <travis@travisluong.com>
2024-11-02 15:14:53 -0700

    reusable layout for docs

--- docs/scripts/build.ts
+++ docs/scripts/build.ts
@@ -4,17 +4,31 @@ import fs from "fs";
 import { getReadme, getTableOfContents } from "../lib/markdown";
 import packageJson from "../../package.json";
 
+Handlebars.registerPartial(
+  "layout",
+  fs.readFileSync(path.join(process.cwd(), "docs/templates/layout.hbs"), "utf8")
+);
+
+Handlebars.registerHelper("layout", function (options) {
+  const layout = Handlebars.partials["layout"];
+  const template = Handlebars.compile(layout, { noEscape: true });
+  // @ts-ignore
+  const content = options.fn(this); // Renders the inner content of the current template
+  // @ts-ignore
+  return template({ ...this, body: content });
+});
+
 async function main() {
   const readme = await getReadme();
   const toc = await getTableOfContents();
   const version = packageJson["version"];
 
   renderTemplate({
-    inputPath: "index.html.hbs",
+    inputPath: "index.hbs",
     outputPath: "docs/dist/index.html",
   });
   renderTemplate({
-    inputPath: "docs.html.hbs",
+    inputPath: "docs.hbs",
     outputPath: "docs/dist/docs.html",
     data: {
       readme: readme,
