61f8467ef01658441d6001938cdd8666aebad98d
Travis Luong <travis@travisluong.com>
2024-11-27 18:20:41 -0800

    config subcommand

--- index.ts
+++ index.ts
@@ -31,6 +31,8 @@ import packageShadrizzJson from "./package-shadrizz.json";
 import packageJson from "./package.json";
 import { pkDependencies } from "./lib/pk-strategy";
 import { ADD_ON_REGISTRY, getAddOnHelpText } from "./lib/add-on-registry";
+import path from "path";
+import os from "os";
 
 const PINNED_NEXTJS_VERSION = packageShadrizzJson.dependencies["next"];
 
@@ -539,5 +541,41 @@ program
 
     processor.printCompletionMessage();
   });
+
+// config top level command
+
+const config = new Command("config").description("configuration");
+
+const configDir = path.join(os.homedir(), ".shadrizz");
+const configFile = path.join(configDir, "config.json");
+
+if (!fs.existsSync(configDir)) {
+  fs.mkdirSync(configDir, { recursive: true });
+}
+
+config
+  .command("set-api-key <key>")
+  .description("set the api key")
+  .action((key) => {
+    const configData = { apiKey: key };
+    fs.writeFileSync(configFile, JSON.stringify(configData, null, 2), "utf8");
+    log.green("API key saved successfully.");
+  });
+
+config
+  .command("get-api-key")
+  .description("get the stored API key")
+  .action(() => {
+    if (fs.existsSync(configFile)) {
+      const configData = JSON.parse(fs.readFileSync(configFile, "utf8"));
+      console.log("Stored API Key:", configData.apiKey);
+    } else {
+      console.log(
+        "No API Key found. Use `config set-api-key <key>` to set one."
+      );
+    }
+  });
+
+program.addCommand(config);
 
 program.parse();
