00acbd9764f7c302800dbce31cb3f18c874c3b85
Travis Luong <travis@travisluong.com>
2024-11-28 18:57:40 -0800

    call scaffold api, render output with boxen

--- commands/ai-command.ts
+++ commands/ai-command.ts
@@ -1,8 +1,21 @@
 import { input } from "@inquirer/prompts";
 import { Command } from "commander";
 import { getApiKey } from "../lib/auth";
+import { z } from "zod";
+import boxen from "boxen";
 
 export const aiCommand = new Command("ai");
+
+const tablesSchema = z.object({
+  tables: z
+    .object({
+      tableName: z.string(),
+      columns: z
+        .object({ columnName: z.string(), dataType: z.string() })
+        .array(),
+    })
+    .array(),
+});
 
 aiCommand
   .summary("ai commands")
@@ -18,7 +31,19 @@ aiCommand
     const value = await input({ message: "What would you like to build?" });
     const res = await fetch("http://localhost:3000/api/ai/scaffold", {
       headers: { "Api-Key": apiKey },
+      body: JSON.stringify({ idea: value }),
+      method: "POST",
     });
     const json = await res.json();
-    console.log(json);
+    const validatedFields = tablesSchema.safeParse(json.result);
+    if (!validatedFields.success) {
+      throw new Error("data returned from api is invalid");
+    }
+    for (const table of validatedFields.data?.tables) {
+      let columnText = "";
+      for (const column of table.columns) {
+        columnText += column.columnName + ":" + column.dataType + "\n";
+      }
+      console.log(boxen(columnText, { title: table.tableName }));
+    }
   });
