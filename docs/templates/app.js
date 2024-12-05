c020cd1428ea833de73f214b56ca6bf31e48ece4
Travis Luong <travis@travisluong.com>
2024-12-04 17:36:20 -0800

    add copy btn to each pre element

--- docs/templates/app.js
+++ docs/templates/app.js
@@ -18,4 +18,26 @@ document.addEventListener("DOMContentLoaded", () => {
       shadrizzDocs.classList.remove("hidden");
     }
   });
+
+  // add copy btn to each pre element
+  const preEls = document.querySelectorAll("pre");
+
+  preEls.forEach((el) => {
+    el.style = "position:relative;";
+    const div = document.createElement("div");
+    div.style = "text-align:right;";
+    const copyBtn = document.createElement("button");
+    copyBtn.innerHTML = "copy";
+    copyBtn.style =
+      "text-align:right; border: 1px solid gray; padding-right: 5px; padding-left: 5px;";
+    copyBtn.addEventListener("click", (e) => {
+      e.target.innerHTML = "copied!";
+      navigator.clipboard.writeText(el.querySelector("code").innerText);
+      setTimeout(() => {
+        copyBtn.innerHTML = "copy";
+      }, 5000);
+    });
+    div.appendChild(copyBtn);
+    el.prepend(div);
+  });
 });
