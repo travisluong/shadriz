8abde30cf9b0d8e3d907eea7312f9ef08a4b79fa
Travis Luong <travis@travisluong.com>
2024-12-14 22:23:41 -0800

    add types for better-sqlite3

--- db-packages/better-sqlite3-package-strategy.ts
+++ db-packages/better-sqlite3-package-strategy.ts
@@ -12,7 +12,7 @@ export class BetterSqlite3PackageStrategy implements DbPackageStrategy {
   shadcnComponents: string[] = [];
   dialect: DbDialect = "sqlite";
   dependencies: string[] = ["better-sqlite3"];
-  devDependencies: string[] = [];
+  devDependencies: string[] = ["@types/better-sqlite3"];
 
   constructor(opts: ShadrizzConfig) {
     this.opts = opts;
